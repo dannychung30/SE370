78a25a036cc5ecb5c178e93fcddada03bec061bf
Danny Chung <dannychung6158@gmail.com>
2023-05-01 15:59:06 -0700

    inital commit

---
+++ BankBuddy/src/bankBuddy/LoginPage.java
@@ -0,0 +1,40 @@
+package bankBuddy;
+import javax.swing.*;
+
+public class LoginPage {
+	
+	JLabel username_label, password_label;
+	JTextField username, password;
+	JButton login_button;
+	
+	LoginPage() {
+		JFrame f = new JFrame("Login");
+		
+		username_label = new JLabel("Username:");
+		username_label.setBounds(150, 70, 200, 50);
+		username = new JTextField("", 255);
+		username.setBounds(150, 100, 200, 50);
+		password_label = new JLabel("Password:");
+		password_label.setBounds(150, 145, 200, 50);
+		password = new JTextField("", 255);
+		password.setBounds(150, 175, 200, 50);
+		login_button = new JButton("Login");
+		login_button.setBounds(200, 250, 100, 50);
+		
+		f.add(username);
+		f.add(username_label);
+		f.add(password);
+		f.add(password_label);
+		f.add(login_button);
+		
+		f.setSize(500, 500);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new LoginPage();
+	}
+
+}
--- lab3/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author dannychung
- *
- */
-module lab3 {
-}
