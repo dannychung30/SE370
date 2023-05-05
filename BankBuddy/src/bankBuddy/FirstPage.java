b1e305c86ef9bca14597161409fb720f3ed6ed13
Danny Chung <dannychung6158@gmail.com>
2023-05-04 19:37:06 -0700

    Utilized GridBagLayout and GridBagConstraints for layout

--- BankBuddy/src/bankBuddy/FirstPage.java
+++ BankBuddy/src/bankBuddy/FirstPage.java
@@ -1,4 +1,5 @@
 package bankBuddy;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
@@ -10,21 +11,35 @@ public class FirstPage {
 	JLabel welcome_msg;
 	
 	FirstPage() {	
+
+		GridBagConstraints gbc = new GridBagConstraints();
 		
 		JFrame f = new JFrame("Bank Buddy");
-		login_btn = new JButton("Login");
-		signup_btn = new JButton("Create New Account");
-		
-		welcome_msg = new JLabel("<html>Welcome to DCA Bank Buddy!<br>Please select from the following options.</html>");
-		welcome_msg.setHorizontalAlignment(JLabel.CENTER);
-		
-		f.add(welcome_msg);
-		f.add(login_btn);
-		f.add(signup_btn);
-		f.setVisible(true);
+
 		f.setLayout(new GridBagLayout());
-		f.setSize(500, 500);
-				
+		f.setSize(500, 200);
+		
+		//// FIRST COLUMN ////
+		welcome_msg = new JLabel("<html>Welcome to DCA"
+				+ " Bank Buddy!<br>Please select from the following options.</html>");
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weighty = 5;
+		f.add(welcome_msg, gbc);
+		
+		login_btn = new JButton("Login");
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.weighty = 0;
+		f.add(login_btn, gbc);
+		
+		signup_btn = new JButton("Create New Account");
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.weighty = 2;
+		f.add(signup_btn, gbc);
+		
+		f.setVisible(true);
 		
 	}
 
