fb02e000b2a33ae562ff67861e8030275544593d
Danny Chung <dannychung6158@gmail.com>
2023-05-08 15:21:22 -0700

    added menu bar to hold all account action buttons

--- BankBuddy/src/bankBuddy/HomePage.java
+++ BankBuddy/src/bankBuddy/HomePage.java
@@ -1,32 +1,74 @@
 package bankBuddy;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class HomePage {
 	
 	JLabel greeting, checking, saving;
-	JButton sign_out;
+	JMenuItem transfer, deposit, withdraw, sign_out;
 	
 	HomePage() {
-		JFrame f = new JFrame("Welcome, ");
+		JFrame mainFrame = new JFrame("Welcome, ");
+	
+		JPanel mainPanel = new JPanel();
+		
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu accountActions = new JMenu("Account Actions");
+		
+		JMenuItem transfer = new JMenuItem("Transfer");
+		JMenuItem deposit = new JMenuItem("Deposit");
+		JMenuItem withdraw = new JMenuItem("Withdraw");
+		JMenuItem sign_out = new JMenuItem("Sign Out");
+		
+		mainFrame.setLayout(new BorderLayout());
+		mainFrame.setSize(400, 400);
+		
+		mainPanel.setLayout(new GridBagLayout());
+		
+		menuBar.setLayout(new BorderLayout());
+		menuBar.setPreferredSize(new Dimension(400, 50));
+		
 		GridBagConstraints gbc = new GridBagConstraints();
 		
-		f.setLayout(new GridBagLayout());
-		f.setSize(500, 500);
-			
-		//JLabel greeting = new JLabel("Welcome, ");
-		JLabel checking = new JLabel("Checking");
-		f.add(checking, gbc);
+		// checking account
+		JLabel checking = new JLabel("<html><strong>Checking</strong></html>");
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		mainPanel.add(checking, gbc);
 		
-		JLabel saving = new JLabel("Saving");
-		f.add(saving, gbc);
+		// saving account
+		JLabel saving = new JLabel("<html><strong>Saving</strong></html>");
+		gbc.gridx = 1;
+		gbc.gridy = 2;
+		mainPanel.add(saving, gbc);
 		
-		JButton sign_out = new JButton("Sign Out");
-		f.add(sign_out, gbc);
+		mainFrame.add(mainPanel, BorderLayout.NORTH);
+		mainFrame.add(menuBar, BorderLayout.SOUTH);
 		
-		f.setVisible(true);	
+		menuBar.add(accountActions, BorderLayout.WEST);
+		menuBar.add(sign_out, BorderLayout.EAST);
+		
+		accountActions.add(transfer);
+		accountActions.add(deposit);
+		accountActions.add(withdraw);
+		
+		mainFrame.setVisible(true);	
+		
+		// go back to first page frame when clicking sign out
+		sign_out.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new FirstPage();
+				mainFrame.dispose();
+			}
+		});
 	}
 
 	public static void main(String[] args) {
