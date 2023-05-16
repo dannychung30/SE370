deb0c3129cc154e5bed3d52588634f64ee854e71
Danny Chung <dannychung6158@gmail.com>
2023-05-15 19:55:03 -0700

    checking to see if form is empty and if passwords match

--- BankBuddy/src/bankBuddy/CreateAccount.java
+++ BankBuddy/src/bankBuddy/CreateAccount.java
@@ -6,13 +6,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.*;
 import java.sql.Statement;
+import java.util.Arrays;
+
 import javax.swing.*;
 
 import com.mysql.cj.jdbc.Driver;
 
 public class CreateAccount {
 	
-	JLabel username_label, password_label, confirm_password_label, email_label;
+	JLabel username_label, password_label, confirm_password_label, email_label, stmt_complete_msg;
 	JTextField username, email;
 	JButton create_button, back_btn;
 	JPasswordField password, confirm_password;
@@ -46,7 +48,10 @@ public class CreateAccount {
 		
 		back_btn = new JButton("<- Back");
 		back_btn.setBounds(10, 10, 75, 25);
-	
+		
+		stmt_complete_msg = new JLabel("Account created!\nGo back to login.");
+		stmt_complete_msg.setBounds(140, 325, 250, 50);
+		stmt_complete_msg.setVisible(false);
 		
 		f.add(email_label);
 		f.add(email);
@@ -58,7 +63,7 @@ public class CreateAccount {
 		f.add(confirm_password);
 		f.add(create_button);
 		f.add(back_btn);
-		
+		f.add(stmt_complete_msg);		
 		f.setSize(500, 500);
 		f.setLayout(null);
 		f.setVisible(true);
@@ -74,21 +79,31 @@ public class CreateAccount {
 		create_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
+					// connecting to database
 					Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/SE370", "root", "password30");
 					Statement stmt = con.createStatement();	
 					String query = "INSERT INTO users VALUES('"+username.getText()+"', '"+password.getText()+"', '"+email.getText()+"')";
-					stmt.executeUpdate(query);
-					System.out.println("Statement made");
-					//ResultSet results = stmt.executeQuery(query);
-//					
-//					while (results.next()) {
-//						String username = results.getString("username");
-//						String password = results.getString("password");
-//						String email = results.getString("email");
-//						System.out.println(username + "   " + password + "   " + email);
-//					}
-				} catch(Exception e1) {
-					System.out.print(e1);
+					// checking if all fields of form are empty 
+					if (username.getText().isEmpty() == true || email.getText().isEmpty() == true || password.getText().isEmpty() == true) {
+						String error = "Please fill out all fields of the form";
+						JOptionPane.showMessageDialog(f, error, "", JOptionPane.ERROR_MESSAGE);
+					} else {
+						// checking if password and confirm passwords are equal
+						if (Arrays.equals(password.getPassword(), confirm_password.getPassword())) {
+							stmt.executeUpdate(query);	
+							create_button.setVisible(false);
+							stmt_complete_msg.setVisible(true);
+							email.setText(null);
+							username.setText(null);
+							password.setText(null);
+							confirm_password.setText(null);
+						} else {
+							String pswd_no_match = "Passwords do not match";
+							JOptionPane.showMessageDialog(f, pswd_no_match, "", JOptionPane.ERROR_MESSAGE);
+						}
+					}
+				} catch(Exception error) {
+					System.out.print(error);
 				}
 			}
 		});
